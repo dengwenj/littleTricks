527ec239aef4ef808ac765858697dc27201fca51
dengwenj <927561237@qq.com>
2022-12-18 17:42:52 +0800

    播放暂停

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useEffect, useRef } from 'react'
+import { useRef, useState } from 'react'
 import ylsh from './music/571215937.mp3'
 
 function App() {
+  const [isPlay, setIsPlay] = useState(false)
   const audioRef = useRef<HTMLAudioElement>(null)
-
+  
+  const handlePlay = () => {
+    if (!isPlay) {
+      audioRef.current?.play()
+      setIsPlay(true)
+      return
+    }
+    audioRef.current?.pause()
+    setIsPlay(false)
+  }
   return (
     <div className="App">
-      <audio ref={audioRef} autoPlay loop controls src={ylsh}></audio>
+      <button onClick={handlePlay}>播放/暂停</button>
+      <audio ref={audioRef} autoPlay loop src={ylsh}></audio>
     </div>
   );
 }
